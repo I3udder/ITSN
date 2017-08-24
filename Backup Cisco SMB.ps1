674ea479981af0469258167499929f051028dc72
Jeroen Vercoulen <J.Vercoulen@itsn.nl>
2017-08-24 14:08:33 +0200

    sample file
    Used for switch list

--- Backup Cisco SMB.ps1
+++ Backup Cisco SMB.ps1
@@ -4,10 +4,18 @@
 # (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 # iex (New-Object Net.WebClient).DownloadString("https://gist.github.com/darkoperator/6152630/raw/c67de4f7cd780ba367cccbc2593f38d18ce6df89/instposhsshdev")
 
+$Switches = Import-Csv sample.csv
+
+ForEach ($Switch in $Switches) {
+
 $time = get-date -format yyyyMMdd-HHmmss 
-$session = New-SSHSession -ComputerName "192.168.32.17" -Credential (Get-Credential)
+
+$password = convertto-securestring -String $Switch.pass -AsPlainText -force
+$credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist $Switch.user,$password
+
+$session = New-SSHSession -ComputerName $Switch.ipaddress -Credential $credentials
 $stream = $session.Session.CreateShellStream("dumb", 0, 0, 0, 0, 1000)
-$stream.Write("copy run scp://cisco:cisco@172.16.50.101/itsnveasw10-" + $time + ".conf`n")
+$stream.Write("copy run scp://cisco:cisco@172.16.50.101/"+ $Switch.hostname  +"-" + $time + ".conf`n")
 #Cisco Catalyst Switches require 3 enters
 #Start-Sleep -s 5
 #$stream.Write("`n")
@@ -15,4 +23,9 @@ $stream.Write("copy run scp://cisco:cisco@172.16.50.101/itsnveasw10-" + $time + 
 #$stream.Write("`n")
 #Start-Sleep -s 5
 #$stream.Write("`n")
+#Start-Sleep -s 30
 Remove-SSHSession $session
+
+
+
+}
