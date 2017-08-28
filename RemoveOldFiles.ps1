2348bb76a95bcc2dcd5501965df60446caa978cb
Jeroen Vercoulen <J.Vercoulen@itsn.nl>
2017-08-28 15:00:18 +0200

    remove old backup files

---
+++ RemoveOldFiles.ps1
@@ -0,0 +1,35 @@
+#----- define parameters -----#
+#----- get current date ----#
+$Now = Get-Date
+$time = get-date -format yyyyMMdd-HHmmss
+#----- define amount of days ----#
+$Days = "14"
+#----- define folder where files are located ----#
+$TargetFolder = "C:\SSH-Root\Backup"
+#----- define extension ----#
+$Extension = "*.conf"
+#----- define LastWriteTime parameter based on $Days ---#
+$LastWrite = $Now.AddDays(-$Days)
+#----- define log file for delete actions
+$path = "C:\SSH-Root\Backup\Switch-Backup-" + $time + ".txt"
+
+
+Start-Transcript -Path $path -NoClobber
+ 
+#----- get files based on lastwrite filter and specified folder ---#
+$Files = Get-Childitem $TargetFolder -Include $Extension -Recurse | Where {$_.LastWriteTime -le "$LastWrite"}
+ 
+foreach ($File in $Files) 
+    {
+    if ($File -ne $NULL)
+        {
+        write-host "Deleting File $File" -ForegroundColor "DarkRed"
+        Remove-Item $File.FullName | out-null
+        }
+    else
+        {
+        Write-Host "No more files to delete!" -foregroundcolor "Green"
+        }
+    }
+
+Stop-Transcript
