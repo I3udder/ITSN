e57c8dadeb8efae293d61627fa50222a2172dc0c
Jeroen Vercoulen <J.Vercoulen@itsn.nl>
2017-08-28 14:28:09 +0200

    dded Logging via Transcript and auto install Posh-SSH

--- Backup Cisco SMB.ps1
+++ Backup Cisco SMB.ps1
@@ -3,12 +3,22 @@
 # Minimal Powershell Version 3
 # (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
 # iex (New-Object Net.WebClient).DownloadString("https://gist.github.com/darkoperator/6152630/raw/c67de4f7cd780ba367cccbc2593f38d18ce6df89/instposhsshdev")
+# Added Logging via Transcript and auto install Posh-SSH
 
 $Switches = Import-Csv sample.csv
+$time = get-date -format yyyyMMdd-HHmmss
+$path = "C:\<Backup DIR>\Switch-Backup-" + $time + ".txt"
+Start-Transcript -Path $path -NoClobber
+
+If ( ! (Get-module Posh-SSH )) { 
+
+iex (New-Object Net.WebClient).DownloadString("https://gist.github.com/darkoperator/6152630/raw/c67de4f7cd780ba367cccbc2593f38d18ce6df89/instposhsshdev")
+
+}
 
 ForEach ($Switch in $Switches) {
 
-$time = get-date -format yyyyMMdd-HHmmss 
+Write-Host "Backup of" $Switch.hostname "Started`r`n"
 
 $password = convertto-securestring -String $Switch.pass -AsPlainText -force
 $credentials = new-object -typename System.Management.Automation.PSCredential -argumentlist $Switch.user,$password
@@ -17,15 +27,14 @@ $session = New-SSHSession -ComputerName $Switch.ipaddress -Credential $credentia
 $stream = $session.Session.CreateShellStream("dumb", 0, 0, 0, 0, 1000)
 $stream.Write("copy run scp://"+ $Switch.scpuser +":"+ $Switch.scppass +"@vpn.itsn.nl/"+ $Switch.hostname  +"-" + $time + ".conf`n")
 #Cisco Catalyst Switches require 3 enters
-#Start-Sleep -s 5
-#$stream.Write("`n")
-#Start-Sleep -s 5
-#$stream.Write("`n")
-#Start-Sleep -s 5
-#$stream.Write("`n")
-#Start-Sleep -s 30
+Start-Sleep -s 5
+$stream.Write("`n")
+Start-Sleep -s 5
+$stream.Write("`n")
+Start-Sleep -s 5
+$stream.Write("`n")
+Start-Sleep -s 30
 Remove-SSHSession $session
-
-
-
+Write-Host "Backup of" $Switch.hostname "Finished`r`n"
 }
+Stop-Transcript 
