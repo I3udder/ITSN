00df3a20e13e3e33e409f4deebb05d5460fab8a4
Jeroen Vercoulen <J.Vercoulen@itsn.nl>
2017-08-29 14:19:01 +0200

    Removed not needed comment

--- Backup Cisco SMB.ps1
+++ Backup Cisco SMB.ps1
@@ -1,8 +1,5 @@
-# Install-Module Posh-SSH first time use required
 # Cisco SMB command ip ssh password-auth must be entered in the comfiguration of the device. If the device doesn't support the command the device needs to be upgraded to a later version of the firmware.
 # Minimal Powershell Version 3
-# (new-object Net.WebClient).DownloadString("http://psget.net/GetPsGet.ps1") | iex
-# iex (New-Object Net.WebClient).DownloadString("https://gist.github.com/darkoperator/6152630/raw/c67de4f7cd780ba367cccbc2593f38d18ce6df89/instposhsshdev")
 # Added Logging via Transcript and auto install Posh-SSH
 
 $Switches = Import-Csv sample.csv
