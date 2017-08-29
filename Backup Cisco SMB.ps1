2362bc341a98c8680a6ad20b15dbeacc1620ab5f
Jeroen Vercoulen <J.Vercoulen@itsn.nl>
2017-08-29 09:55:40 +0200

    Changes Outpur

--- Backup Cisco SMB.ps1
+++ Backup Cisco SMB.ps1
@@ -10,6 +10,8 @@ $time = get-date -format yyyyMMdd-HHmmss
 $path = "C:\<Backup DIR>\Switch-Backup-" + $time + ".txt"
 Start-Transcript -Path $path -NoClobber
 
+import-module Posh-SSH
+
 If ( ! (Get-module Posh-SSH )) { 
   If ( ! (Get-Module PsGet)) {
    iex (New-Object Net.WebClient).DownloadString("https://gist.github.com/darkoperator/6152630/raw/c67de4f7cd780ba367cccbc2593f38d18ce6df89/instposhsshdev")
@@ -22,7 +24,7 @@ If ( ! (Get-module Posh-SSH )) {
 
 ForEach ($Switch in $Switches) {
 
-$Output = "Backup of"+ $Switch.hostname +"Started"
+$Output = "Backup of "+ $Switch.hostname +" Started"
 Write-Output $Output
 
 $password = convertto-securestring -String $Switch.pass -AsPlainText -force
@@ -30,7 +32,7 @@ $credentials = new-object -typename System.Management.Automation.PSCredential -a
 
 $session = New-SSHSession -ComputerName $Switch.ipaddress -Credential $credentials -AcceptKey
 $stream = $session.Session.CreateShellStream("dumb", 0, 0, 0, 0, 1000)
-$stream.Write("copy run scp://"+ $Switch.scpuser +":"+ $Switch.scppass +"@vpn.itsn.nl/"+ $Switch.hostname  +"-" + $time + ".conf`n")
+$stream.Write("copy run scp://"+ $Switch.scpuser +":"+ $Switch.scppass +"@<Host FQDN or IP>/"+ $Switch.hostname  +"-" + $time + ".conf`n")
 #Cisco Catalyst Switches require 3 enters
 Start-Sleep -s 5
 $stream.Write("`n")
@@ -41,7 +43,7 @@ $stream.Write("`n")
 Start-Sleep -s 30
 Remove-SSHSession $session
 
-$Output = "Backup of"+ $Switch.hostname +"Finished"
+$Output = "Backup of "+ $Switch.hostname +" Finished"
 Write-Output $Output
 }
 Stop-Transcript 
