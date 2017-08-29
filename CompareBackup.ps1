31df99fb83f1cd3d037c836af0d93a37a09e5f20
Jeroen Vercoulen <J.Vercoulen@itsn.nl>
2017-08-29 14:09:23 +0200

    Addev variables from mail-message

--- CompareBackup.ps1
+++ CompareBackup.ps1
@@ -4,6 +4,9 @@ $TargetFolder = "C:\SSH-Root\Backup\"
 $Extension = "*.conf"
 $TempFile = "C:\temp\Changelog.txt"
 $path = "C:\SSH-root\Backup\Compare-" + $time + ".txt"
+$From = "<email address>"
+$To = "<Email Address>"
+$SMTPServer = "<Mail-Server>"
 
 Start-Transcript -Path $path -NoClobber
 
@@ -11,6 +14,7 @@ $Files = get-childitem $TargetFolder -Recurse -include $Extension | Where-Object
 
 ForEach ($File in $Files) {
  #Write-Host $File.name.Substring(0,$File.name.Length-21)
+ #When used with the Backup Cisco SMB script the last 21 characters are the extension and the date/time notification
  $Filename = $File.name.Substring(0,$File.name.Length-21) +"-"+ $timeYesterday
  $Include = ""
  $Include += $Filename
@@ -29,7 +33,7 @@ ForEach ($File in $Files) {
      $compare | Out-File $TempFile
      $Subject = "Change found in "+ $File.name
      $Body = "For complete changelog see attachement. We compared "+ $File.name +" and "+ $file2.name
-     Send-MailMessage -From "helpdesk@itsn.nl" -To "j.vercoulen@itsn.nl" -Subject $Subject -Body $Body -Attachments $TempFile -dno onSuccess, onFailure -SmtpServer "192.168.99.20"
+     Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -Attachments $TempFile -dno onSuccess, onFailure -SmtpServer $SMTPServer
      Remove-Item $TempFile
    }
    Write-Output "Finished"
